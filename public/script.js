183bbd55e3cc0e444fb297cbb432285a1d70755d
Alandrian Surya Tantra <alandriansuryatantra@gmail.com>
2025-01-13 19:44:13 +0700

    v1.0.0

---
+++ public/script.js
@@ -0,0 +1,9 @@
+// import Typed from 'typed.js';
+
+// const typed = new Typed('#element', {
+//     strings: ['Welcome BioAlgo.'],
+//     typeSpeed: 50,
+//     smartBackspace: true,
+// });
+
+import 'animate.css';
